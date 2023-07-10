9a89229f2b45df6be0a3ba3e5accb6c41029248f
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-10 19:26:24 +0300

    traitement Login

--- application/controllers/ControlleurLogin.php
+++ application/controllers/ControlleurLogin.php
@@ -7,5 +7,21 @@ class ControlleurLogin extends CI_Controller {
 	{
 		$this->load->view('Login');
 	}
+	public function Login()
+        {
+            $this->load->library('session');
+            $this->load->helper('url');
+            $mdp = $this->input->post('mdp');
+            $utilisateur = new utilisateur(null,$email,$mdp);
+            $utilisateur = $user->login();
+            if($utilisateur != null){
+                $this->session->set_userdata('id_utilisateur',$user->getId_utilisateur());
+            }
+            if($this->session->userdata('id_utilisateur') == null){
+                $this->load->view('completion');
+            }else{
+                redirect('');
+            }
+        }
 	
 }
