3e95aebc7c991ea2f5e2e7e0e092b22cab6bcf1b
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-10 18:32:35 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,12 +1,12 @@
 create database Projet_S4;
 use Projet_S4;
-create table user(
+create table utilisateur(
     idUser int primary key auto_increment,
     nom varchar(100) not null,
     email varchar(100) not null unique,
     mdp varchar(100) not null,
 );
-
+insert into 
 
 
 
