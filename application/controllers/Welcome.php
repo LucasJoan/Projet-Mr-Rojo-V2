bc77a08634fe52c3ef63d39df0d6f97c01b41f64
Lucas <Lucas.rajoelina@gmail.com>
2023-07-10 20:09:07 +0300

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->helper('url');
@@ -26,16 +11,16 @@ class Welcome extends CI_Controller {
 		
 		// Récupérer les valeurs des champs
 		$nom = $this->input->post('nom');
-		$prenom = $this->input->post('prenom');
+		$prenom = $this->input->post('Prenom');
 		$mail = $this->input->post('email');
 		$pass = $this->input->post('mdp');
 
 		if (!empty($nom)) {
 			$data = array(
 				'nom' => $nom,
-				'prenom' => $prenom,
+				'Prenom' => $prenom,
 				'email' => $mail,
-				'mdp' => $mdp,
+				'mdp' => $pass,
 			);
 			
 			$this->InsertUtilisateur->insert_Utilisateur($data);
