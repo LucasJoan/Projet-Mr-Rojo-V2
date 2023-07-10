6301cd5f16d9053563d8476f47780e846acfd4a7
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-10 18:46:50 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -4,5 +4,5 @@ create table utilisateur(
     id_utilisateur  int primary key auto_increment,
     nom varchar(100) not null,
     email varchar(100) not null unique,
-    mdp varchar(100) not null,
+    mdp varchar(100) not null
 );
