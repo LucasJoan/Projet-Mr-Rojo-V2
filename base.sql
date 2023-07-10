3f22dc8b4d9dff98b2400eaf16e02fb448633fbc
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-10 21:46:27 +0300

    rectification base

--- base.sql
+++ base.sql
@@ -3,6 +3,7 @@ use Projet_S4;
 create table utilisateur(
     id_utilisateur  int primary key auto_increment,
     nom varchar(100) not null,
+    Prenom varcahr(100) not null,
     email varchar(100) not null unique,
     mdp varchar(100) not null
 );
