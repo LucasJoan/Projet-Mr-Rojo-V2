c0d588e25e6c2503fd0f25c3fc139a769d57959a
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-11 10:29:04 +0300

    completion controlleur et models

--- application/controllers/controlleurCompletion.php
+++ application/controllers/controlleurCompletion.php
@@ -1,1 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class controlleurCompletion extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->helper('url');
+		$this->load->database();
+		$this->load->model('insert_Completion');
+		
+		// Récupérer les valeurs des champs
+		$nom = $this->input->post('nom');
+		$prenom = $this->input->post('Prenom');
+		$mail = $this->input->post('email');
+		$pass = $this->input->post('mdp');
+
 		if (!empty($nom)) {
+			$data = array(
+				'nom' => $nom,
+				'prenom' => $prenom,
+				'email' => $mail,
+				'mdp' => $pass,
+			);
+			
+			$this->InsertUtilisateur->insert_Utilisateur($data);
+		}
+		
+		$this->load->view('index');
+	}
+
+}
