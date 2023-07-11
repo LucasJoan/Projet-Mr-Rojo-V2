9716c591ca806a7b96b50358bf4cbb35f52f733b
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-11 16:55:11 +0300

    re-rectificatio completion

--- application/views/completion.php
+++ application/views/completion.php
@@ -9,6 +9,7 @@
 <body>
   <div class="container">
     <h1>Complétion du Profil Utilisateur</h1>
+
     <form id="profileForm" action="controlleurCompletion/index" method="POST">
       <label for="genre">Genre :</label>
       <select id="genre" name="genre">
@@ -67,6 +68,34 @@
           mysqli_close($conn);
         ?>
       </select>
+<!-- utilisateur -->
+<label for="utilisateur">utilisateur :</label>
+      <select id="utilisateur" name="utilisateur">
+      <?php
+          // Connexion à la base de données
+          $conn = mysqli_connect("localhost", "root", "", "Projet_S4");
+
+          // Vérification de la connexion
+          if (!$conn) {
+            die("Erreur de connexion à la base de données : " . mysqli_connect_error());
+          }
+
+          // Récupérer les options de la table 'genres' dans la base de données
+          $utilisateur = "SELECT * FROM utilisateur";
+          $result_utilisateur = mysqli_query($conn, $utilisateur);
+
+          // Générer les options du select
+          while ($row1 = mysqli_fetch_assoc($result_utilisateur)) {
+            echo '<option value="' . $row1['utilisateur'] . '">' . $row1['nom'] . '</option>';
+            // echo $genre;
+          }
+
+          // Fermer la connexion à la base de données
+          mysqli_close($conn);
+        ?>
+      </select>
+
+
 
       <button type="submit">Enregistrer</button>
     </form>
