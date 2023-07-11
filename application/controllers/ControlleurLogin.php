8eae1da5522d9bc1e4b8c7d3f1447486e8fec110
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-11 14:28:20 +0300

    login

--- application/controllers/ControlleurLogin.php
+++ application/controllers/ControlleurLogin.php
@@ -4,21 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class ControlleurLogin extends CI_Controller {
 
     public function __construct() {
-        parent::__construct();
-        $this->load->library('session');
-        $this->load->helper('url');
-        $this->load->model('ModelsLogin');
+      parent::__construct();
+      $this->load->library('session');
+      $this->load->helper('url');
+	$this->load->database();
+      $this->load->model('ModelsLogin');
     }
 
     public function login() {
         // Récupérez les données du formulaire de connexion
         $email = $this->input->post('email');
-        $password = $this->input->post('password');
+        $password = $this->input->post('mdp');
 
         // Vérifiez les informations d'identification de l'utilisateur
         $user = $this->ModelsLogin->getUserByEmail($email);
 
-        if ($user && password_verify($password, $user->password)) {
+        if ($user && password_verify($mdp, $user->mdp)) {
             // Les informations d'identification sont valides, connectez l'utilisateur
             $this->session->set_userdata('user_id', $user->id);
             redirect('Completion'); // Redirigez vers la page de tableau de bord ou une autre page appropriée
@@ -26,7 +27,6 @@ class ControlleurLogin extends CI_Controller {
             // Les informations d'identification sont invalides, affichez un message d'erreur
             $data['error'] = 'Email ou mot de passe incorrect.';
             $this->load->view('login', $data);
-            redirect('Completion'); // Redirigez vers la page de tableau de bord ou une autre page appropriée
 
         }
     }
