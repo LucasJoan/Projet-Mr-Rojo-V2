566df4119e1e1ac69d72b0984c3dd563b5e1ae76
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-11 07:44:51 +0300

    base completion

--- base.sql
+++ base.sql
@@ -7,3 +7,29 @@ create table utilisateur(
     email varchar(100) not null unique,
     mdp varchar(100) not null
 );
+create table genre(
+    id_genre int primary key auto_increment,
+    nom_genre varchar(10),
+    id_utilisateur int,
+    foreign  key genre (id_utilisateur) references utilisateur(id_utilisateur)
+);
+
+create table objectif(
+    id_objectif int primary key auto_increment,
+    objectif varchar(100),
+    id_utilisateur int,
+    foreign  key objectif (id_utilisateur) references utilisateur(id_utilisateur)
+);
+
+create table taille(
+    id_taille int primary key auto_increment,
+    taille decimal(8.3),
+    id_utilisateur int,
+    foreign  key taille (id_utilisateur) references utilisateur(id_utilisateur)
+);
+create table poids(
+    id_poids int primary key auto_increment,
+    poids decimal(8.3),
+    id_utilisateur int,
+    foreign  key poids (id_utilisateur) references utilisateur(id_utilisateur)
+);
