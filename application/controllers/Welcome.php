72be9380b880ced65615163d7e377cd0b2ea5b6d
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-11 11:19:21 +0300

    welcom

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,7 +11,7 @@ class Welcome extends CI_Controller {
 		
 		// Récupérer les valeurs des champs
 		$nom = $this->input->post('nom');
-		$prenom = $this->input->post('prenom');
+		$prenom = $this->input->post('Prenom');
 		$mail = $this->input->post('email');
 		$pass = $this->input->post('mdp');
 
